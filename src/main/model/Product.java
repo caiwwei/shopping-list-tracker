0dc557e51a87c434e2469e7805629065c84931e3
Sarah <sarahliang13013@gmail.com>
2024-02-15 11:37:12 -0800

    added TODO methods

--- src/main/model/Product.java
+++ src/main/model/Product.java
@@ -1,5 +1,7 @@
 package model;
 
+// represents a single product
+
 public class Product {
     private String name;     // name of product
     private int price;       // price of product
@@ -16,23 +18,63 @@ public class Product {
         this.link = productLink;
     }
 
+    // getter
     public String getName() {
         return name;
     }
 
+    // getter
     public int getPrice() {
         return price;
     }
 
+    // getter
     public int getDiscount() {
         return discount;
     }
 
+    // getter
     public String getCode() {
         return code;
     }
 
+    // getter
     public String getLink() {
         return link;
+    }
+
+    // REQUIRES: nothing
+    // MODIFIES: this
+    // EFFECTS: changes name of product
+    public void inputName() {
+        // TODO
+    }
+
+    // REQUIRES: nothing
+    // MODIFIES: this
+    // EFFECTS: changes price of product
+    public void inputPrice() {
+        // TODO
+    }
+
+    // REQUIRES: nothing
+    // MODIFIES: this
+    // EFFECTS: changes discount of product
+    public void inputDiscount() {
+        // TODO
+    }
+
+    // REQUIRES: nothing
+    // MODIFIES: this
+    // EFFECTS: changes code of product
+    public void inputCode() {
+        // TODO
+    }
+
+    // REQUIRES: nothing
+    // MODIFIES: this
+    // EFFECTS: changes link of product
+    public void inputLike() {
+        // TODO
     }
 }
--- src/main/model/ShoppingList.java
+++ src/main/model/ShoppingList.java
@@ -2,6 +2,8 @@ package model;
 
 import java.util.ArrayList;
 
+// represents a single shopping list with products
+
 public class ShoppingList {
     private int amount;                        // amount of items in shopping list
     private String name;                       // name of the shopping list
@@ -14,16 +16,33 @@ public class ShoppingList {
         this.products = null;
     }
 
+    // getter
     public int getAmount() {
         return amount;
     }
 
+    // getter
     public String getName() {
         return name;
     }
 
+    // getter
     public ArrayList<Product> getProducts() {
         return products;
+    }
+
+    // REQUIRES: nothing
+    // MODIFIES: this
+    // EFFECTS: add a product to products
+    public void addProduct(Product product){
+        // TODO
+    }
+
+    // REQUIRES: product exists in products
+    // MODIFIES: this
+    // EFFECTS: remove a product from products
+    public void deleteProduct(Product product){
+        // TODO
     }
 
 }
